7de16fbf66cd5441a4374b26a29b4692a126c9cd
bryutus <bryutus@gmail.com>
2018-10-14 19:10:42 +0900

    Update to 0.1.1 version

--- kivu.rb
+++ kivu.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Kivu < Formula
   homepage "https://github.com/bryutus/kivu"
-  url "https://github.com/bryutus/kivu/releases/download/v0.1.0/kivu_darwin_amd64.tar.gz"
-  sha256 "07cc202cfd4ff01bda0ff508ed98f814d365fccc4ce04bd9ba983930d5e84d92"
+  url "https://github.com/bryutus/kivu/releases/download/v0.1.1/kivu_darwin_amd64.tar.gz"
+  sha256 "c3da32fd670db2e8365e85ab975455e31e2d4f7179261973f246203e70db56ad"
   head "https://github.com/bryutus/kivu.git"
-  version "v0.1.0"
+  version "v0.1.1"
 
   def install
     bin.install "kivu"
